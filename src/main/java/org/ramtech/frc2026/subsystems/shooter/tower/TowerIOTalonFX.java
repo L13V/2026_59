c26c246b64eeb4ec41d471bd8427d57fe58a07d2
L13V <liev@dorfman.net>
2026-02-17 21:53:17 -0500

    add talonfx stuff for the subsystems

---
+++ src/main/java/org/ramtech/frc2026/subsystems/shooter/tower/TowerIOTalonFX.java
@@ -0,0 +1,63 @@
+package org.ramtech.frc2026.subsystems.shooter.tower;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import org.ramtech.frc2026.Constants;
+import org.ramtech.frc2026.Constants.TowerConstants;
+
+public class TowerIOTalonFX implements TowerIO {
+  // Motors
+  private final TalonFX towerMotor =
+      new TalonFX(TowerConstants.towerMotorId, Constants.CANBus); // Main Motor
+
+  // Configuration
+  private final TalonFXConfiguration towerConfig = new TalonFXConfiguration();
+  private boolean towerConfigured = false;
+
+  // Control Methods
+  private final VoltageOut voltageOut = new VoltageOut(0); // Control Method
+  private final VelocityVoltage velocityVoltage = new VelocityVoltage(0);
+
+  public TowerIOTalonFX() {
+    // Complete the config
+    towerConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    towerConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+  }
+
+  @Override
+  public void updateInputs(TowerIOInputs inputs) {
+    // Configuration
+    inputs.towerConnected = towerMotor.isConnected(); // Detect connected
+    if (!towerConfigured && inputs.towerConnected) { // Configure motor
+      towerMotor.getConfigurator().apply(towerConfig);
+      towerConfigured = true;
+    }
+
+    inputs.towerConfigured = towerConfigured;
+
+    inputs.towerAppliedVoltage = towerMotor.getMotorVoltage().getValueAsDouble();
+
+    inputs.towerVelocity = towerMotor.getVelocity().getValueAsDouble();
+
+    inputs.towerSupplyCurrentAmps = towerMotor.getSupplyCurrent().getValueAsDouble();
+  }
+
+  @Override
+  public void applyOutputs(TowerIOOutputs outputs) {
+    switch (outputs.mode) {
+      case OFF:
+        towerMotor.stopMotor();
+        break;
+
+      case VOLTAGE:
+        towerMotor.setControl(voltageOut.withOutput(outputs.voltage));
+        break;
+      case VELOCITY:
+        towerMotor.setControl(velocityVoltage.withVelocity(outputs.velocity));
+    }
+  }
+}
